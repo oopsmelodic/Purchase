af9c9ac0c8cd2c4aaf4ed7b5bd69fa95e78886eb
e_bragin <e_bragin@n-08266.inline.local>
2016-03-22 17:06:49 +0300

    Profile controller,view and model

---
+++ application/models/model_profile.php
@@ -0,0 +1,44 @@
+<?php
+
+class Model_Profile extends Model{
+
+    public function get_profile_data($id){
+        include_once './php/conn.php';
+        $query=mysqli_query(GetMyConnection(),"SELECT em.fullname as fullname, em.id as id, em.email as email, dp.name as department, em.position as position, rs.name as role_name FROM employee as em".
+            " Left Join departments as dp on em.department_id=dp.id".
+            " Left Join roles as rs on em.role_id=rs.id".
+            " Where em.id = ".$id.";");
+        if ($query){
+            if (mysqli_num_rows($query)){
+                while ($row = mysqli_fetch_assoc($query)) {
+                    $rows[] = $row;
+                }
+                return $rows;
+            }else{
+                return null;
+            }
+        }else{
+            print_r(mysqli_error(GetMyConnection()));
+        }
+    }
+    
+     public function get_profile_statistics($id){
+        include_once './php/conn.php';
+        $query=mysqli_query(GetMyConnection(),"SELECT em.fullname as fullname, em.id as id, em.email as email, dp.name as department, em.position as position, rs.name as role_name FROM employee as em".
+            " Left Join departments as dp on em.department_id=dp.id".
+            " Left Join roles as rs on em.role_id=rs.id".
+            " Where em.id = ".$id.";");
+        if ($query){
+            if (mysqli_num_rows($query)){
+                while ($row = mysqli_fetch_assoc($query)) {
+                    $rows[] = $row;
+                }
+                return $rows;
+            }else{
+                return null;
+            }
+        }else{
+            print_r(mysqli_error(GetMyConnection()));
+        }
+    }
+}
