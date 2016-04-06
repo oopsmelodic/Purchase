6c9b6092ce389e68ab4b05831375ab94c89ec7fe
e_bragin <e_bragin@n-08266.inline.local>
2016-04-06 12:01:11 +0300

    Added print feature for show_iom

--- application/views/show_view.php
+++ application/views/show_view.php
@@ -1,3 +1,4 @@
+
 <div id="wrapper">
 
     <!-- Navigation -->
@@ -63,48 +64,52 @@
                                 <form class="form-horizontal " autocomplete="off" id="purchase_form"  data-toggle="validator">
                                     <fieldset>
                                         <div class="col-lg-12 shadow" style="padding:20px 20px 0px 20px;">
-                                            <legend id ="iom_id" iom_id="<?php echo $data['id']; ?>"><i class="glyphicon glyphicon-send" aria-hidden="true"></i> IOM №<?php echo $data['id']; ?> </legend>
-                                            <div class="form-group col-lg-12">
-                                                <h4 class="col-lg-6"><legend id="user_id" user_id="<?php echo $data['employee_id'] ?>"><span class="label label-default">Initiator:</span>  <?php echo $data['fullname']; ?> </legend></h4>
-                                                <h4 class="col-lg-6"><legend id="department_id" department_id="<?php echo $data['department_id'] ?>"><span class="label label-default">Department:</span> <?php echo $data['department']; ?></legend></h4>
-                                            </div>
-                                            <div class="col-lg-12 form-group">
-                                                <div class="col-lg-6">
-                                                    <h4 class="col-lg-6"><span class="label label-default">Budgets:</span></h4>
-                                                    <table id="budgets"></table>
-                                                    <br>
-                                                    <h4 class="col-lg-4"><span class="label label-default">Expense size:</span></h4>
-                                                    <div class="col-lg-8" align="center" style="border: 1px solid #ccc; border-radius:4px; background: #F5F5F5; padding: 5px;"><?php echo ($data['costsize']!=="0")? $data['costsize']:"-"; ?></div>
+                                            <div id="printer">
+                                                <link href="/css/print.css" rel="stylesheet" media="print" type="text/css"/>
+                                                <legend id ="iom_id" iom_id="<?php echo $data['id']; ?>"><i class="glyphicon glyphicon-send" aria-hidden="true"></i> IOM №<?php echo $data['id']; ?> <div class="btn btn-success"  onclick="window.print();"  style="margin-bottom: 5px;"><i class="fa fa-print"></i></div></legend>
+                                                <div class="form-group col-lg-12">
+                                                    <h4 class="col-lg-4"><legend id="user_id" user_id="<?php echo $data['employee_id'] ?>"><span class="label label-default">Initiator:</span>  <?php echo $data['fullname']; ?> </legend></h4>
+                                                    <h4 class="col-lg-4"><legend id="department_id" department_id="<?php echo $data['department_id'] ?>"><span class="label label-default">Department:</span> <?php echo $data['department']; ?></legend></h4>
+                                                    <h4 class="col-lg-4"><legend><span class="label label-default">Date:</span> <?php echo date('d.m.Y', strtotime($data['time_stamp'])); ?></legend></h4>
+                                                </div>
+                                                <div class="col-lg-12 form-group">
+                                                    <div class="col-lg-6">
+                                                        <h4 class="col-lg-6"><span class="label label-default">Budgets:</span></h4>
+                                                        <table id="budgets"></table>
 
-                                                </div>
-                                                <div class="col-lg-6">
-                                                    <h4 class="col-lg-6"><span class="label label-default">Signers:</span></h4>
-                                                    <table id="signers"></table>
+                                                        <?php
+                                                        echo ($data['costsize'] !== "0") ? '<h4 class="col-lg-12"><legend><span class="label label-default">Expense size:</span> '
+                                                                . $data['costsize'] . '</legend></h4>' : "";
+                                                        ?>
+                                                    </div>
+                                                    <div class="col-lg-6">
+                                                        <h4 class="col-lg-6"><span class="label label-default">Signers:</span></h4>
+                                                        <table id="signers"></table>
 
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <h3 class="col-lg-4"><span class="label label-default"> Purchase name: </span></h3>
-                                                <div class="col-lg-12">
-                                                    <!--<textarea id="purchase_text" rows="4" style="width:100%; max-width: 100%; " class="form-control" required disabled="true"></textarea>-->
-                                                    <div style="border: 1px solid #ccc; border-radius:4px; background: #F5F5F5; padding: 15px;"><?php echo $data['name']; ?></div>
+                                                <div id="pagebreaker" class="form-group">
+                                                    <h3 class="col-lg-4"><span class="label label-default"> Purchase name: </span></h3>
+                                                    <div class="col-lg-12">
+                                                        <!--<textarea id="purchase_text" rows="4" style="width:100%; max-width: 100%; " class="form-control" required disabled="true"></textarea>-->
+                                                        <div class="iominfo" style="border: 1px solid #ccc; border-radius:4px; background: #F5F5F5; padding: 15px;"><?php echo $data['name']; ?></div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <h3 class="col-lg-6"><span class="label label-default"> Substantiation: </span></h3>
-                                                <div class="col-lg-12">
-                                                    <!--<textarea name="summernote" id="summernote" cols="10" rows="10"></textarea>-->
-                                                    <div style="border: 1px solid #ccc; border-radius:4px; background: #F5F5F5; padding: 15px;"><?php echo $data['substantation']; ?></div>
+                                                <div class="form-group">
+                                                    <h3 class="col-lg-6"><span class="label label-default"> Substantiation: </span></h3>
+                                                    <div class="col-lg-12">
+                                                        <!--<textarea name="summernote" id="summernote" cols="10" rows="10"></textarea>-->
+                                                        <div class="iominfo" style="border: 1px solid #ccc; border-radius:4px; background: #F5F5F5; padding: 15px;"><?php echo $data['substantation']; ?></div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <h3 class="col-lg-4"><span class="label label-default"> Files: </span></h3>
-                                                <div class="col-lg-12">
-                                                    <table id="files"></table>
+                                                <div class="form-group">
+                                                    <h3 class="col-lg-4"><span class="label label-default"> Files: </span></h3>
+                                                    <div class="col-lg-12">
+                                                        <table id="files"></table>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-
                                     </fieldset>
                                 </form>
                             </div>
