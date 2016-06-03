25ff5e4b9ce8aee5c15fdef765c72f5b96f421c2
oopsmelodic <oops.melodic@gmail.com>
2016-06-03 17:56:45 +0300

    Bug Fix Budgets;

--- application/controllers/controller_purchase.php
+++ application/controllers/controller_purchase.php
@@ -17,18 +17,19 @@ class Controller_Purchase extends Controller
 //            print_r($data);
             $roles = $this->model->get_roles_data();
             $budgets = $this->model->get_budgets();
+            $mapping = $this->model->get_mapping();
+            $brand = $this->model->get_brand();
             $flip_roles = Array();
             $flip_budgets = Array();
             foreach($roles as $value){
                 $flip_roles[$value['role_name']][] = '<option  data-content="<h5>'.$value['fullname'].'<small>'.$value['department'].'</small></h5>"  emp_id="'.$value['id'].'" role_power="'.$value['role_power'].'">'.$value['id'].' </option>';
             }
             foreach($budgets as $value){
-                $flip_budgets[$value['budget_type_name']][] = '<option  data-content="<h5>'.$value['budget_name'].'<small>'.$value['planed_cost'].'</small></h5>" planed_cost="'.$value['planed_cost'].'">'.$value['id'].' </option>';
+                $flip_budgets[$value['budget_type_name']][] = '<option  data-content="<h5>'.$value['budget_name'].' - '.date('M/Y',strtotime($value['date_time'])).'<small>'.(is_null($value['cur_sum']) ? $value['planed_cost'] : $value['cur_sum']).'</small></h5>" planed_cost="'.$value['planed_cost'].'">'.$value['id'].' </option>';
             }
-            foreach($flip_budgets as $key=>$value){
-                $data['budgets'].='<optgroup label="'.$key.'"'.'data-max-options="">'.implode('',$value).'</optgroup>';
+            foreach($flip_budgets as $key=>$value) {
+                $data['budgets'] .= '<optgroup label="' . $key . '"' . 'data-max-options="">' . implode('', $value) . '</optgroup>';
             }
-//            $data['budgets'] = $flip_budgets;
             $data['roles'] = $flip_roles;
             $this->view->generate('purchase_view.php', 'template_view.php', $data);
         }
